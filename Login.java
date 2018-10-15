a952c22cfe857b2c53d904833b8b62d45c36d6ba
dd1331 <35798203+dd1331@users.noreply.github.com>
2018-10-16 01:56:17 +0900

    Update Login.java
    바깥 쪽 for - loop 추가

--- Login.java
+++ Login.java
@@ -6,7 +6,7 @@ import javax.swing.JOptionPane;
 import java.io.*;
 import java.util.*;
 
-class Login 
+class Login
 {
 	JFrame f;
 	JLabel la,laId,laPw;
@@ -24,7 +24,7 @@ class Login
 		f = new JFrame();
 		f.setLayout(new BorderLayout());
 		EtchedBorder eb = new EtchedBorder();
-		
+
 		la = new JLabel();
 		la.setBorder(eb);
 		f.add(la);
@@ -41,20 +41,18 @@ class Login
 		pwPanel.add(pw);
 		pw.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-				loginCheck();    
+				loginCheck();
             }
         });
-
 
 		loginPanel = new JPanel();
 		b = new JButton("로그인");
 		loginPanel.add(b);
 		b.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-    
+
             }
         });
-		
 
 		f.add(idPanel,"North");
 		f.add(pwPanel,"Center");
@@ -66,9 +64,8 @@ class Login
 		f.setLocation(700,300);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setVisible(true);
-
 		// -----------------------   종료 옵션 창 ------------------------------------------
-		f.addWindowListener(new WindowAdapter(){  
+		f.addWindowListener(new WindowAdapter(){
 			public void windowClosing(WindowEvent e){
 				int answer = JOptionPane.showConfirmDialog(
 					f, "종료할까요?", "선택", JOptionPane.OK_CANCEL_OPTION);
@@ -80,37 +77,39 @@ class Login
 			}
 		});
 	}
-
-	
-	// -----------------------   로그인 시 ID / PW 확인 창 ------------------------------------------  
-	void loginCheck(){ 
+	// -----------------------   로그인 시 ID / PW 확인 창 ------------------------------------------
+	void loginCheck(){
 		try{
-			BufferedReader br = new BufferedReader(new FileReader("wList.txt"));	
+			BufferedReader br = new BufferedReader(new FileReader("wList.txt"));
 			String s;
-				while((s=br.readLine()) != null){
+			outer:
+			for(int i = 0;i<=1;i++){
+				while((s=br.readLine())!=null){
 					String [] sArray  = s.split("\\*");
 					loginid = String.valueOf(sArray[1]);
 					loginpass = String.valueOf(sArray[2]);
-				
+
 					if(id.getText().equals(loginid) && new String (pw.getPassword()).equals(loginpass)){
 						JOptionPane.showMessageDialog(null, "로그인 완료!");
 							 boolean check = true;
 							 if(check){
 								f.setVisible(false);
-								break;
+								break outer;
 							 }
-					}else {
-							JOptionPane.showMessageDialog(null, "아이디 또는 비밀번호를 다시 확인하세요.", "로그인 실패", JOptionPane.ERROR_MESSAGE);  
-							id.setText("");
-							pw.setText("");
-							break;
-					 }  // 작성한 ID / PW 초기화 ------------------- 근데 커서가 ID로 안가고 PW로 감.. ㅠ
+					}
+				}if(i==0)br = new BufferedReader(new FileReader("mList.txt"));
+				else if(i==1){
+					JOptionPane.showMessageDialog(null, "아이디 또는 비밀번호를 다시 확인하세요.", "로그인 실패", JOptionPane.ERROR_MESSAGE);
+					pw.setText("");
+					id.setText("");
 				}
+
+			}
 			}catch(IOException ie){}
 	}
 
 
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
 		new Login();
 	}
