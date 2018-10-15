35b9ae46f892cdb4b0910cf60714c985deaf9127
NrnJeanieKim <43194930+NrnJeanieKim@users.noreply.github.com>
2018-10-15 15:37:41 +0900

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -1,108 +1,35 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-import javax.imageio.*;
-import java.io.*;
-class GUI extends JFrame{
-  String tinder ="/Users/Sungsu/Desktop/Tinder/";
-  JPanel jp1,jp2,jp3,lower,upper;
-  JButton leftBt,rightBt;
-  JLabel logoLb, photoLb;
-  Load l;
-  String topBanner = tinder+"TopBanner.jpg";
-  String rightImage = tinder+"RightKey.jpg";
-  String leftImage = tinder+"LeftKey.jpg";
-  int counter = 1;
-  ImageIcon img;
-  JLabel proLb;
-  String gender;
-
-  class RightLeft implements ActionListener{
-    RightLeft(){
-      leftBt.addActionListener(this);
-      rightBt.addActionListener(this);
-    }
-    public void actionPerformed(ActionEvent ae){
-      counter++;
-      try{
-        l.pick(counter,gender);
-        System.out.println(l.ptPath);
-        change(l.ptPath);
-      }catch(NullPointerException ne){
-          System.out.println(" 매칭 할 사람이 없다");
-          // jp2.setVisible(false);
-          // JLabel noPic = new JLabel();
-          // noPic.setIcon(new ImageIcon(ImageIO.read(new File(tinder+"NoOne.jpg"))));
-          // JPanel noOne = new JPanel();
-          // noOne.add(noPic);
-          // jp2.add(noOne);
-          change();
-      }
-    }
-  }
-  void change(String ptPath){
-    try{
-      if(gender.equals("female"))photoLb.setIcon(new ImageIcon(ImageIO.read(new File(tinder+"wPhoto/"+ptPath))));
-      else photoLb.setIcon(new ImageIcon(ImageIO.read(new File(tinder+"mPhoto/"+ptPath))));
-      proLb.setText(l.profile);
-    }catch(IOException ie){
-    }
-  }
-  void change(){
-    JLabel noPic = new JLabel();
-    try{
-      noPic.setIcon(new ImageIcon(ImageIO.read(new File(tinder+"NoOne.jpg"))));
-      lower.add(noPic,BorderLayout.CENTER);
-    }catch(IOException ie){
-    }
-  }
-
-  GUI(){
-    try{
-      gender = "female";
-      l = new Load();
-      l.pick(counter,gender);
-      leftBt = new JButton(new ImageIcon(ImageIO.read(new File(leftImage))));
-      rightBt = new JButton(new ImageIcon(ImageIO.read(new File(rightImage))));
-      RightLeft rl = new RightLeft();
-      logoLb = new JLabel(new ImageIcon(ImageIO.read(new File(topBanner))));
-      if(gender.equals("male"))img = new ImageIcon(ImageIO.read(new File(tinder+"mPhoto/"+l.ptPath)));
-      else img = new ImageIcon(ImageIO.read(new File(tinder+"wPhoto/"+l.ptPath)));
-      photoLb= new JLabel(img);
-      proLb = new JLabel(l.profile);
-    }catch(IOException ie){
-    }
-    ////////////TOP//////////////
-    jp1 = new JPanel(new FlowLayout());
-    jp1.add(logoLb);
-    Choose c = new Choose();
-    logoLb.addKeyListener(c.new MyKeyListener());
-    ///////////MIDDLE////////////
-    upper = new JPanel(new FlowLayout());
-    upper.add(proLb);
-    jp2 = new JPanel(new GridLayout(2,1));
-    jp2.add(photoLb);
-    jp2.add(upper);
-    ////////////BOTTOM//////////
-    jp3 = new JPanel(new FlowLayout());
-    jp3.add(leftBt);
-    jp3.add(rightBt);
-    /////////////////////////////
-    lower = new JPanel();
-    lower.setLayout(new BorderLayout());
-    lower.add(jp1,BorderLayout.NORTH);
-    lower.add(jp2,BorderLayout.CENTER);
-    lower.add(jp3,BorderLayout.SOUTH);
-    add(lower);
-    init();
-  }
-  void init(){
-    setTitle("Tinder");
-    setSize(800,500);
-    setLocation(400,400);
-    setVisible(true);
-  }
-  public static void main(String[] args) {
-    GUI g = new GUI();
-  }
-}
+class AskChat extends JFrame{ //팝업창 띄우기 위한 클래스. Choose외부.
+	 Choose cs; //띄우고 나서 cs.ask = false로 만들어줘야 함.
+	 JPanel upPanel, middlePanel, downPanel; //매치, 하트, 대화/계속고르기 부착되는 JPanel
+	 JLabel match, heart; //"It's Match!", 하트그림
+	 JButton sendButton, keepButton; //"send a message", "Keep Playing"
+	 Container cp;
+	 AskChat(Choose cs){
+		 this.cs = cs;
+		 init();			  
+	  }
+	 void init(){
+		cp = getContentPane();
+		upPanel = new JPanel(); middlePanel = new JPanel(); downPanel = new JPanel();
+		cp.add(upPanel, BorderLayout.NORTH);
+		cp.add(middlePanel, BorderLayout.CENTER);
+		cp.add(downPanel, BorderLayout.SOUTH);
+		match = new JLabel("It's a Match!");
+		heart = new JLabel("heart"); //하트아이콘 넣기★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
+		sendButton = new JButton("Send a Message");
+		keepButton = new JButton("Keep Playing");
+		//패널 내부는 FLOWLAYOUT으로바꿔서해야할듯??
+		upPanel.add(match);
+		middlePanel.add(heart);
+		downPanel.add(sendButton); downPanel.add(keepButton);
+		setUI();
+	  }
+	 void setUI(){
+		//setTitle("대화하시겠습니까?");타이틀꼭필요한가???
+		setSize(350, 500);
+		setLocation(500,300);
+		setVisible(true);
+		setResizable(true);
+		//setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //이 세 버튼은 없앨수없나
+	}
+	  }
