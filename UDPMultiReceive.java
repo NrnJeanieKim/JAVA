5937bb5c7ac302fa3123975c283a593b87cf6a68
NrnJeanieKim <43194930+NrnJeanieKim@users.noreply.github.com>
2018-10-16 10:43:00 +0900

    Add files via upload
    TClient.java로 돌리면 기능합니닷

---
+++ UDPMultiReceive.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
+public class UDPMultiReceive extends Thread {
+	private InetAddress inetAddress = null;
+	private int portNumber = 0;
+	private MulticastSocket multicastSocket = null;
+	private DatagramPacket datagramPacket = null;
+
+	public UDPMultiReceive(InetAddress tmpInetAddress, int tmpportNumber) {
+		inetAddress = tmpInetAddress;
+		portNumber = tmpportNumber;
+	}
+	public void run() {
+		byte[] buffer = new byte[512];// 1. Data를 받을 버퍼 생성
+		try {
+			multicastSocket = new MulticastSocket(portNumber);// 2. DatagramPacket을 받기 위한 Socket 생성
+			multicastSocket.joinGroup(inetAddress);// 3. 그룹 등록 - 통신 가능하게 함
+			while(true) {	// 메시지 계속 받음
+	// 4. Data를 받을 Packet 생성
+				datagramPacket = new DatagramPacket(buffer, buffer.length);
+	// 5. 멀티캐스트에 존재하는 메시지 받음
+				multicastSocket.receive(datagramPacket);
+	// 6. 수신된 메시지 출력
+				String msg = new String(datagramPacket.getData(), 0, datagramPacket.getLength());
+				System.out.println("받은 메시지 ==> " + msg);
+			}
+		} catch (IOException e) {
+			System.err.println(e);
+			System.exit(0);
+			} finally {
+				multicastSocket.close();
+			}
+		}
+}
---
+++ UDPMultiSend.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class UDPMultiSend extends Thread {
+	private InetAddress inetAddress = null;
+	private int portNumber = 0;
+	private String strId = "";
+	private DatagramSocket datagramSocket = null;
+	private DatagramPacket datagramPacket = null;
+	private BufferedReader br = null;
+
+	public UDPMultiSend(InetAddress tmpInetAddress, int tmpportNumber, String tmpstrId) {
+		inetAddress = tmpInetAddress;
+		portNumber = tmpportNumber;
+		strId = tmpstrId;
+	}
+	public void run() {
+		///////////////////////////////
+		// 1. Data를 보낼 버퍼 생성
+		///////////////////////////////
+		byte[] buffer = new byte[512];
+		///////////////////////////////
+		try {	// 2. Socket 열기
+			datagramSocket = new DatagramSocket(); 
+			br = new BufferedReader(new InputStreamReader(System.in)); // 3. Server로 메시지 전송을 위한 입력 스트림 생성
+			while(true) {// 메시지 계속 전송
+				System.out.println("메세지를 입력하시오 : ");// 4. 메시지 입력
+				String msg = "[" + strId + "]" + br.readLine();
+				buffer = msg.getBytes(); // 스트림을 바이트 어레이
+				datagramPacket = new DatagramPacket(buffer, buffer.length, inetAddress, portNumber);// 6. DatagramPacket에 각 정보를 담고 전송
+				datagramSocket.send(datagramPacket);
+			}
+		} catch(IOException ie) {	//System.err.println(ie);
+			System.out.println("send 오류");
+		} finally {
+			datagramSocket.close();
+		}
+	}
+}
+
